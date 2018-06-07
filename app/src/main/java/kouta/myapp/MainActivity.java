2f5e45f482be57e3f57fccac050845f1d44f13f2
Kouta Takamatsu <bb1016181@gmail.com>
2018-06-07 20:36:39 +0900

    add options

--- app/src/main/java/kouta/myapp/MainActivity.java
+++ app/src/main/java/kouta/myapp/MainActivity.java
@@ -1,28 +1,26 @@
 package kouta.myapp;
 
-import android.support.annotation.Nullable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import kouta.myapp.TimetableFragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Menu mainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         TimetableFragmentPagerAdapter adapter = new TimetableFragmentPagerAdapter(getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.viewPager);
@@ -31,16 +29,29 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
-        final MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_timetable_reset) {
+            return true;
+        }else if(id==R.id.menu_options){
+            return true;
+        }
 
-        return super.onCreateOptionsMenu(menu);
+        return super.onOptionsItemSelected(item);
     }
 }
