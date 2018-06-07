bd00ab5114eceacc4878d0fa5901640b6a8ecd7d
Kouta Takamatsu <bb1016181@gmail.com>
2018-06-07 14:09:35 +0900

    add Viewpager and Tablayout

--- app/src/main/java/kouta/myapp/MainActivity.java
+++ app/src/main/java/kouta/myapp/MainActivity.java
@@ -1,13 +1,46 @@
 package kouta.myapp;
 
-import android.app.Activity;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity {
+    private Menu mainMenu;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimetableFragmentPagerAdapter adapter = new TimetableFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
